4068edc4febd8b53cae2bf0774b6b85c83fa9ab0
HongWonSeok <85995027+HongWonSeok@users.noreply.github.com>
2024-05-01 18:43:45 +0900

    Add files via upload

---
+++ model/loss.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+# def Weighted_CE_Loss():
+    
+#     return torch.nn.CrossEntropyLoss(weight=torch.tensor([0.1, 0.4, 0.2, 0.5, 0.2]))
+
+
+def Weighted_CE_Loss(args_weight):
+    
+    return nn.CrossEntropyLoss(weight=args_weight)
+
+
