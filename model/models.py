7dc536139aab358a654089158ee403bcaf085759
HongWonSeok <85995027+HongWonSeok@users.noreply.github.com>
2024-05-01 18:45:25 +0900

    Update models.py

--- model/models.py
+++ model/models.py
@@ -1,75 +1,11 @@
-from math import sqrt
-
-import einops
-from einops.layers.torch import Reduce
 import torch
 import torch.nn as nn
 
 
 # CNN
-class ECG1DCNN(nn.Module):
-    def __init__(self, num_class=5):
-        super(ECG1DCNN, self).__init__()
-
-        self.conv1 = nn.Sequential(
-            nn.Conv1d(1, 16, kernel_size = 3, stride = 1, padding=0),
-            nn.BatchNorm1d(16),
-            nn.ReLU(),
-            nn.AvgPool1d(kernel_size=3, stride=2, padding=1)
-        )
-
-        self.conv2 = nn.Sequential(
-            nn.Conv1d(16, 32, kernel_size=3, stride=1),
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
-            nn.AvgPool1d(kernel_size=2, stride=2, padding=1)
-        )
 
 
-        self.fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(143968, 500),  # Note: 'im_shape[0]//8' depends on the length of your input sequence and the pooling layers.
-            nn.ReLU(),
-            nn.Linear(500, num_class)
-        )
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.fc(x)
-        return x
-    
-
-# 우리가 일단 쓸 SimpleMLP
 # 일단 구조상관없이 생성
-
-class SIMPLEMLP(nn.Module):
-    def __init__(self, num_class=2):
-        super(SIMPLEMLP, self).__init__()
-        
-        self.fc1 = nn.Linear(187, 128,bias=False)
-        self.bn1 = nn.BatchNorm1d(128) 
-        self.relu1 = nn.ReLU()
-        self.dropout1 = nn.Dropout(0.5) 
-        # self.fc2 = nn.Linear(18500, 17000,bias=False)
-        # self.bn2 = nn.BatchNorm1d(17000) 
-        # self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(0.5) 
-        self.out = nn.Linear(128, num_class,bias=False)
-        self.relu3 = nn.ReLU()
-
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.bn1(x)  
-        x = self.relu1(x)
-        x = self.dropout1(x)
-        # x = self.fc2(x)
-        # x = self.bn2(x)  
-        # x = self.relu2(x)
-        # x = self.dropout2(x)
-        x = self.out(x)
-    
 
 class SIMPLEMLP_Icentia(nn.Module):
     def __init__(self, num_class=2):
@@ -79,12 +15,8 @@ class SIMPLEMLP_Icentia(nn.Module):
         self.bn1 = nn.BatchNorm1d(600) 
         self.relu1 = nn.ReLU()
         self.dropout1 = nn.Dropout(0.5) 
-        # self.fc2 = nn.Linear(18500, 17000,bias=False)
-        # self.bn2 = nn.BatchNorm1d(17000) 
-        # self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(0.5) 
         self.out = nn.Linear(600, num_class,bias=False)
-        self.relu3 = nn.ReLU()
+
 
 
     def forward(self, x):
@@ -92,10 +24,6 @@ class SIMPLEMLP_Icentia(nn.Module):
         x = self.bn1(x)  
         x = self.relu1(x)
         x = self.dropout1(x)
-        # x = self.fc2(x)
-        # x = self.bn2(x)  
-        # x = self.relu2(x)
-        # x = self.dropout2(x)
         x = self.out(x)
     
         
@@ -109,10 +37,6 @@ class SIMPLEMLP_CinC(nn.Module):
         self.bn1 = nn.BatchNorm1d(20000) 
         self.relu1 = nn.ReLU()
         self.dropout1 = nn.Dropout(0.5) 
-        # self.fc2 = nn.Linear(18500, 17000,bias=False)
-        # self.bn2 = nn.BatchNorm1d(17000) 
-        # self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(0.5) 
         self.out = nn.Linear(20000, num_class,bias=False)
 
 
@@ -122,141 +46,11 @@ class SIMPLEMLP_CinC(nn.Module):
         x = self.bn1(x)  
         x = self.relu1(x)
         x = self.dropout1(x)
-        # x = self.fc2(x)
-        # x = self.bn2(x)  
-        # x = self.relu2(x)
-        # x = self.dropout2(x)
         x = self.out(x)
 
         return x
     
 
-
-
-# 원본 ECG 신호를 변환하는 embedding layer
-class LinearEmbedding(nn.Sequential):
-
-    # input으로 
-    def __init__(self, input_channels, output_channels) -> None:
-        super().__init__(*[
-            nn.Linear(input_channels, output_channels),
-            nn.LayerNorm(output_channels),
-            nn.GELU()
-        ])
-        
-        # cls_token이름의 learnable paramter생성
-        self.cls_token = nn.Parameter(torch.randn(1, output_channels))
-
-    def forward(self, x):
-        # 여기서 input x가 nn.Linear, nn.LayerNorm, nn.GELU를 거쳐져서 embedding됨.
-        embedded = super().forward(x)
-        # cls_token을 batch크기에 맞게 복제하고, (토큰 n, 임베딩 차원 e)를 (batch , n , e)형태로 변경, 
-        # 이후 cls_token 과 embedding된 embedded를 연결함. 그러면 cls_token이 임베딩의 맨앞에 위치하게됨.
-        return torch.cat([einops.repeat(self.cls_token, "n e -> b n e", b=x.shape[0]), embedded], dim=1)
-
-
-class MLP(nn.Sequential):
-    def __init__(self, input_channels, expansion=4):
-        super().__init__(*[
-            nn.Linear(input_channels, input_channels * expansion),
-            nn.GELU(),
-            nn.Linear(input_channels * expansion, input_channels)
-        ])
-
-
-class ResidualAdd(torch.nn.Module):
-    def __init__(self, block):
-        super().__init__()
-        self.block = block
-
-    def forward(self, x):
-        return x + self.block(x)
-
-
-# attention 계산.
-class MultiHeadAttention(torch.nn.Module):
-    def __init__(self, embed_size, num_heads, attention_store=None):
-        super().__init__()
-        self.queries_projection = nn.Linear(embed_size, embed_size)
-        self.values_projection = nn.Linear(embed_size, embed_size)
-        self.keys_projection = nn.Linear(embed_size, embed_size)
-        self.final_projection = nn.Linear(embed_size, embed_size)
-        self.embed_size = embed_size
-        self.num_heads = num_heads
-
-    def forward(self, x):
-        assert len(x.shape) == 3
-        keys = self.keys_projection(x)
-        values = self.values_projection(x)
-        queries = self.queries_projection(x)
-        keys = einops.rearrange(keys, "b n (h e) -> b n h e", h=self.num_heads)
-        queries = einops.rearrange(queries, "b n (h e) -> b n h e", h=self.num_heads)
-        values = einops.rearrange(values, "b n (h e) -> b n h e", h=self.num_heads)
-        energy_term = torch.einsum("bqhe, bkhe -> bqhk", queries, keys)
-        divider = sqrt(self.embed_size)
-        mh_out = torch.softmax(energy_term, -1)
-        out = torch.einsum('bihv, bvhd -> bihd ', mh_out / divider, values)
-        out = einops.rearrange(out, "b n h e -> b n (h e)")
-        return self.final_projection(out)
-
-
-# transforemr encoder 정의
-# 위의 ResidualAdd를 통해 2개의 residual이 있고 순차적으로 적용하여 encoderlayer 정의
-class TransformerEncoderLayer(torch.nn.Sequential):
-    def __init__(self, embed_size=768, expansion=4, num_heads=8, dropout=0.1):
-        super(TransformerEncoderLayer, self).__init__(
-            *[
-                
-                ResidualAdd(nn.Sequential(*[
-                    nn.LayerNorm(embed_size),
-                    MultiHeadAttention(embed_size, num_heads),
-                    nn.Dropout(dropout)
-                ])),
-                ResidualAdd(nn.Sequential(*[
-                    nn.LayerNorm(embed_size),
-                    MLP(embed_size, expansion),
-                    nn.Dropout(dropout)
-                ]))
-            ]
-        )
-
-
-class Classifier(nn.Sequential):
-    def __init__(self, embed_size, num_classes):
-        super().__init__(*[
-            Reduce("b n e -> b e", reduction="mean"),
-            nn.Linear(embed_size, embed_size),
-            nn.LayerNorm(embed_size),
-            nn.Linear(embed_size, num_classes)
-        ])
-
-
- 
-# 이게 본체임 일단.
-# input으로는 (batch, element, channel순으로 들어옴.) // (64, 187, 1)
-# train.py에서 모델 입력으로 einops.rearrange(signal, "b c e -> b e c") 형식으로 들어왔기 때문에.
-class ECGformer(nn.Module):
-
-    def __init__(self, num_layers, signal_length, num_classes, input_channels, embed_size, num_heads, expansion) -> None:
-        super().__init__()
-         
-        # num_layers에 대해 TransformerEncoderLayer instance들의 list를 생성. 이것은 nn.ModuleList에 담김.
-        self.encoder = nn.ModuleList([TransformerEncoderLayer(
-            embed_size=embed_size, num_heads=num_heads, expansion=expansion) for _ in range(num_layers)])
-        self.classifier = Classifier(embed_size, num_classes)
-        self.positional_encoding = nn.Parameter(torch.randn(signal_length + 1, embed_size))
-        self.embedding = LinearEmbedding(input_channels, embed_size)
-
-    def forward(self, x):
-        # embedding을 진행하고, cls_token을 추가함.
-        embedded = self.embedding(x)
-
-        # encoder에 저장된 list들에 positional_encoding을 더함.
-        for layer in self.encoder:
-            embedded = layer(embedded + self.positional_encoding)
-
-        # classification
-        return self.classifier(embedded)
 
 
 
